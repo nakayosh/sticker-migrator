2ec18e43a495f7baa075cb9dda64ff3691c3f91a
Neetshin <neetshin@neetsh.in>
2018-01-28 21:49:44 +0530

    Update frontend dependencies

---
+++ resources/views/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-title" content="Sticker Migrater">
+
+    <title>Sticker Migrater</title>
+
+    {{--
+    <meta name="msapplication-config" content="/browserconfig.xml">
+    <meta name="theme-color" content="#1da1f2">
+    <meta name="csrf-token" content="{{ csrf_token() }}" id="csrf-token">
+    --}}
+
+    <link rel="stylesheet" href="{{ mix('main.css', '/packs') }}" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+
+    {{--  <link rel="mask-icon" href="/mask-icon.svg" color="#1da1f2">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180">
+    <link rel="manifest" href="/manifest.json">  --}}
+  </head>
+
+  <body class="" role="application">
+    <div id="root"></div>
+    <script src="{{ mix('/main.js', '/packs') }}"></script>
+  </body>
+
+</html>
---
+++ webpack.config.js
@@ -0,0 +1,111 @@
+const webpack = require('webpack');
+const path    = require('path');
+const { env } = require('process');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+
+const isProd = env.NODE_ENV === 'production';
+
+module.exports = {
+
+  stats: isProd ? 'normal' : { errorDetails: true },
+
+  devtool: !isProd && 'source-map' || '',
+
+  entry: {
+    main: './resources/smigrater/main.js',
+    theme_light: './resources/styles/main.scss',
+  },
+
+  output: {
+    filename: isProd ? '[name]-[chunkhash].js' : '[name].js',
+    chunkFilename: isProd ? '[name]-[chunkhash].js' : '[name].js',
+    path: path.resolve(__dirname, 'public', 'packs'),
+    publicPath: '/packs/',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: isProd,
+                sourceMap: !isProd,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: !isProd,
+              },
+            },
+          ],
+        }),
+      },
+      {
+        test: /\.(jpe?g|png|gif|ttf|otf|eot|svg|woff(2)?)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: isProd ? '[name]-[hash].[ext]' : '[name].[ext]',
+            publicPath: '/packs/',
+          },
+        }],
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.js'],
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+
+    new ExtractTextPlugin({
+      filename: isProd ? '[name]-[contenthash].css' : '[name].css',
+      allChunks: true,
+      publicPath: '/packs/',
+    }),
+
+    new ManifestPlugin({
+      fileName: 'mix-manifest.json',
+      basePath: '/',
+      writeToFileEmit: true,
+    }),
+  ],
+
+};
+
+if ( isProd ) {
+  module.exports.plugins.push([
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      mangle: true,
+      compress: {
+        warnings: false,
+      },
+      output: {
+        comments: false,
+      },
+    }),
+  ]);
+}
