e8f796af61ca8d781fb5016b26fbe5f39a04d9df
Neetshin <neetshin@neetsh.in>
2018-01-28 22:40:23 +0530

    Just make display logo

--- resources/smigrater/features/home/index.js
+++ resources/smigrater/features/home/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
+import Page from '../app/components/page';
+import Content from '../app/components/content';
+
+import Logo from '../../../images/logo_large.png';
+
 export default class Home extends React.PureComponent {
 
   render () {
     return (
-      <div>
-        Laravel + React
-      </div>
+      <Page>
+        <Content>
+          <h1>
+            <span className='invisible'>
+              Sticker Migrater
+            </span>
+
+            <img src={Logo} alt='Sticker Migrater' />
+          </h1>
+        </Content>
+      </Page>
     );
   }
 
