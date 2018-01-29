2b75c080fe439460f9926a19fb2e5c781087cf0a
pinfort <ptg@nijitei.com>
2018-01-29 21:00:49 +0900

    modelに存在していればダウンロードしない

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Lib\Imagedownloader\Line;
+use App\Stpack;
 
 class testController extends Controller
 {
     public function index(Request $request, $stpack_no){
-        $downloader = new Line();
-        $data = $downloader->download('https://store.line.me/stickershop/product/'.$stpack_no.'/ja');
-        return view('test', ['data' => $data]);
+        $packmodel = Stpack::where('stpack_id', $stpack_no);
+        if ($packmodel->exists()) {
+            $stpack = $packmodel->get();
+        } else {
+            $downloader = new Line();
+            $stpack = $downloader->download('https://store.line.me/stickershop/product/'.$stpack_no.'/ja');
+        }
+        return view('test', ['data' => $stpack]);
     }
 }
