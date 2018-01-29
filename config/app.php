9079f9a224cc1ae27608d3ffa8375f53f10ffb98
pinfort <ptg@nijitei.com>
2018-01-29 17:44:27 +0900

    コメントの形式修正

--- config/app.php
+++ config/app.php
@@ -177,7 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        /*
+        /**
          * service provider by pinfort
          */
         Intervention\Image\ImageServiceProvider::class,
@@ -230,7 +230,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-        /*
+        /**
          * custom aliases by pinfort
          */
         'Image' => Intervention\Image\Facades\Image::class,
