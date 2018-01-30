a434418a4461373669cc7e5142368612919b530b
Neetshin <neetshin@neetsh.in>
2018-01-30 22:20:22 +0530

    Fix small error

--- app/Http/Controllers/Api/StpacksController.php
+++ app/Http/Controllers/Api/StpacksController.php
@@ -10,20 +10,21 @@ use App\Stpack;
 class StpacksController extends Controller
 {
     public function getPack(Request $request, $stpack_id){
-        $stpack_model   = Stpack::where('id', $stpack_id);
+        $stpack_model = Stpack::where('id', $stpack_id);
+        $return_code  = 200;
 
         if (!$stpack_model->exists()) {
             $downloader = new Line();
             $download   = $downloader->download($stpack_id);
 
             if (is_array($download)) {
-                $return_code = $download['code'] ?: 200;
+                $return_code = $download['code'];
             }
         }
 
         $stpack_model = Stpack::where('id', $stpack_id)->first();
         $stpack       = $stpack_model::with('stickers')->first();
 
-        return response()->json($stpack, is_null($return_code) ? 500 : $return_code);
+        return response()->json($stpack, $return_code ?: 500);
     }
 }
