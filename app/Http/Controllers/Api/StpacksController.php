c13ce06dd8396347a3dbbe07688532b2fe5dc735
Neetshin <neetshin@neetsh.in>
2018-01-30 22:18:06 +0530

    Gutte実装

--- app/Http/Controllers/Api/StpacksController.php
+++ app/Http/Controllers/Api/StpacksController.php
@@ -10,17 +10,20 @@ use App\Stpack;
 class StpacksController extends Controller
 {
     public function getPack(Request $request, $stpack_id){
-        $packmodel = Stpack::where('id', $stpack_id);
-        $return_code = 200;
-        if (!$packmodel->exists()) {
+        $stpack_model   = Stpack::where('id', $stpack_id);
+
+        if (!$stpack_model->exists()) {
             $downloader = new Line();
-            $download = $downloader->download('https://store.line.me/stickershop/product/'.$stpack_id.'/ja');
+            $download   = $downloader->download($stpack_id);
+
             if (is_array($download)) {
-                $return_code = $download['code'];
+                $return_code = $download['code'] ?: 200;
             }
         }
-        $packmodel = Stpack::where('id', $stpack_id)->first();
-        $stpack = $packmodel::with('stickers')->first();
+
+        $stpack_model = Stpack::where('id', $stpack_id)->first();
+        $stpack       = $stpack_model::with('stickers')->first();
+
         return response()->json($stpack, is_null($return_code) ? 500 : $return_code);
     }
 }
