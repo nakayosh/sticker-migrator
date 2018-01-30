82d51451804cfef5c8ee4233b05f160d91a478ad
Neetshin <neetshin@neetsh.in>
2018-01-30 19:51:46 +0530

    Change route source for api.php & Change thowing arg name for stpack_id

--- app/Http/Controllers/Api/StpacksController.php
+++ app/Http/Controllers/Api/StpacksController.php
@@ -9,17 +9,17 @@ use App\Stpack;
 
 class StpacksController extends Controller
 {
-    public function getPack(Request $request, $stpack_no){
-        $packmodel = Stpack::where('id', $stpack_no);
+    public function getPack(Request $request, $stpack_id){
+        $packmodel = Stpack::where('id', $stpack_id);
         $return_code = 200;
         if (!$packmodel->exists()) {
             $downloader = new Line();
-            $download = $downloader->download('https://store.line.me/stickershop/product/'.$stpack_no.'/ja');
+            $download = $downloader->download('https://store.line.me/stickershop/product/'.$stpack_id.'/ja');
             if (is_array($download)) {
                 $return_code = $download['code'];
             }
         }
-        $packmodel = Stpack::where('id', $stpack_no)->first();
+        $packmodel = Stpack::where('id', $stpack_id)->first();
         $stpack = $packmodel::with('stickers')->first();
         return response()->json($stpack, is_null($return_code) ? 500 : $return_code);
     }
