d498251c907f8d490436a0487b85711aafe6a41c
pinfort <ptg@nijitei.com>
2018-01-31 14:52:54 +0900

    id_str, stpack_id_str取得できるように

--- app/Sticker.php
+++ app/Sticker.php
@@ -11,9 +11,24 @@ class Sticker extends Model
         'id',
     ];
 
+    /**
+     * モデルの配列形態に追加するアクセサ
+     *
+     * @var array
+     */
+    protected $appends = ['id_str', 'stpack_id_str'];
+
     public $incrementing = false;
 
     public function stpack(){
         return $this->belongsTo('App\Stpack');
+    }
+
+    public function getIdStrAttribute(){
+        return (string)$this->id;
+    }
+
+    public function getStpackIdStrAttribute(){
+        return (string)$this->stpack_id;
     }
 }
