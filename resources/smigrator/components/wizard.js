0c3b4a70cf05d3a3d72b7b6ba31745fa3947a440
Neetshin <neetshin@neetsh.in>
2018-02-02 21:42:35 +0530

    Add regexp validation for URL form

--- resources/smigrator/components/wizard.js
+++ resources/smigrator/components/wizard.js
@@ -35,14 +35,14 @@ export default class Wizard extends React.PureComponent {
     return (
       <div className='wizard'>
         <h2>
-          Create a new pack
+          Create a new sticker set
         </h2>
 
         <div className='wizard__form'>
           <input
             className='wizard__input rich-input input'
             type='text'
-            pattern='https:\/\/store\.line\.me\/stickershop\/product\/[0-9]+'
+            pattern='https:\/\/store\.line\.me\/stickershop\/product\/[0-9]+?(\/.*)*'
             placeholder='https://store.line.me/stickershop/product/3897'
             onChange={this.handleChange}
             onKeyDown={this.handleKeyDown}
