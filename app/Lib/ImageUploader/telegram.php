444249b82497efb38140865d44a6c581d93396ae
pinfort <ptg@nijitei.com>
2018-02-02 14:44:21 +0900

    emojisカラムはstring

--- app/Lib/ImageUploader/telegram.php
+++ app/Lib/ImageUploader/telegram.php
@@ -16,7 +16,7 @@ class Telegram
         $stickers = $stpack['stickers'];
         $first_sticker = $stickers[0];
         $uploaded_sticker = $this->api->sticker->uploadStickerFile('app/resized_stickers/'.$first_sticker['id_str']);
-        $sticker_set = $this->api->sticker->createNewStickerSet($this->user_id, $stpack['short_name'], $stpack['name'], $uploaded_sticker['file_id'], implode('', $first_sticker['emojis'] ?? ['☺']));
+        $sticker_set = $this->api->sticker->createNewStickerSet($this->user_id, $stpack['short_name'], $stpack['name'], $uploaded_sticker['file_id'], $first_sticker['emojis'] ?? '☺');
         $first_sticker->file_id = $uploaded_sticker['file_id'];
         $first_sticker->save();
         $uploaded_sticker_count = 1;
@@ -32,6 +32,6 @@ class Telegram
         $uploaded_sticker = $this->api->sticker->uploadStickerFile('app/resized_stickers/'.$sticker['id_str']);
         $sticker->file_id = $uploaded_sticker['file_id'];
         $sticker->save();
-        return $this->api->sticker->addStickerToSet($this->user_id, $sticker_set_name, $uploaded_sticker['file_id'], implode('', $sticker['emojis'] ?? ['☺']));
+        return $this->api->sticker->addStickerToSet($this->user_id, $sticker_set_name, $uploaded_sticker['file_id'], $sticker['emojis'] ?? '☺');
     }
 }
