e48abba6684e068d56215397bb52fd026a255fb3
pinfort <ptg@nijitei.com>
2018-02-04 16:54:02 +0900

    stpackstatus const追加

---
+++ app/Lib/Constants/StpackStatus.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Lib\Constants;
+
+class StpackStatus
+{
+    const DOWNLOADED = 0;
+    const COMPILING  = 1;
+    const UPLOADING  = 2;
+    const UPLOADED   = 3;
+    const FAILED     = 4;
+}
