5a498aa4c5a41826bf6017fcbd440121674242d2
Neetshin <neetshin@neetsh.in>
2018-02-09 21:43:04 +0530

    メンテナーを消したよ :(

--- resources/smigrator/features/home/index.js
+++ resources/smigrator/features/home/index.js
@@ -4,7 +4,6 @@ import Content from '@/features/app/components/content';
 import GeneralHeader from '@/features/general_header';
 import WizardContainer from '@/features/home/containers/wizard_container';
 import RecentStpacks from '@/features/recent_stpacks/containers/recent_stpacks_container';
-import Maintainers from '@/components/maintainers';
 
 export default class Home extends React.PureComponent {
 
@@ -16,7 +15,6 @@ export default class Home extends React.PureComponent {
         <Content>
           <WizardContainer />
           <RecentStpacks />
-          <Maintainers />
         </Content>
       </Page>
     );
--- resources/smigrator/features/mobile_home/index.js
+++ resources/smigrator/features/mobile_home/index.js
@@ -3,7 +3,6 @@ import Page from '@/features/app/components/page';
 import Content from '@/features/app/components/content';
 import GeneralHeader from '@/features/general_header';
 import WizardContainer from '@/features/home/containers/wizard_container';
-import Maintainers from '@/components/maintainers';
 
 export default class MobileHome extends React.PureComponent {
 
@@ -14,7 +13,6 @@ export default class MobileHome extends React.PureComponent {
 
         <Content>
           <WizardContainer />
-          <Maintainers />
         </Content>
       </Page>
     );
